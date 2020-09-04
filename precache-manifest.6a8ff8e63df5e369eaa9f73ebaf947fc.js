0906939017ee20e2a4dcd25656ca5475cd890de2
khill <kalisha.hill@gmail.com>
2020-09-04 14:58:27 -0500

    Updates

---
+++ precache-manifest.6a8ff8e63df5e369eaa9f73ebaf947fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c87a38bb64e4be930611ad66bcdb9a8",
+    "url": "/mytodolist/index.html"
+  },
+  {
+    "revision": "9c4c9bce965bb5b41b42",
+    "url": "/mytodolist/static/js/2.81955aca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mytodolist/static/js/2.81955aca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc481ec8c012b9d2e89e",
+    "url": "/mytodolist/static/js/main.57df12d9.chunk.js"
+  },
+  {
+    "revision": "cbc790be00b6fbc0c0ee",
+    "url": "/mytodolist/static/js/runtime-main.fb697f2e.js"
+  }
+]);
